e592c5913b5538bb92f31e92279d3b33e6c90445
Lucas <lucasbriansilvafagundes@gmail.com>
2025-07-29 22:03:39 -0300

    cards automáticos
    aqui organizamos os componentes para adicionar mais cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,12 @@ function App() {
 
   return (
     <>
-     <Card/>
-     <Card/>
+     <Card nome='Rick Sanchez' foto="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+     <Card nome='Morty Smith' foto="https://rickandmortyapi.com/api/character/avatar/2.jpeg"/>
+     <Card nome='Summer Smith' foto="https://rickandmortyapi.com/api/character/avatar/3.jpeg"/>
+     <Card nome='Johnny Depp' foto="https://rickandmortyapi.com/api/character/avatar/23.jpeg"/>
     </>
+   
   )
 }
 
