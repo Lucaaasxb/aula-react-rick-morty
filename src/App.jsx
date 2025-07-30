79256ceb5f8a4aa5d6e1bd65dd43f5fcc0a7c26e
Lucas <lucasbriansilvafagundes@gmail.com>
2025-07-29 22:23:01 -0300

    infos
    criação de infos para melhor busca

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,33 @@ import './App.css'
 import Card from './card'
 
 function App() {
+  const item1 ={
+    nome:'Rick Sanchez', 
+    foto:'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+  const item2 ={
+    nome:'Morty smith', 
+    foto:'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+    const item3 ={
+    nome:'Summer smith', 
+    foto:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  } 
+  const item4 ={
+    nome:'Johnny Depp', 
+    foto:'https://rickandmortyapi.com/api/character/avatar/23.jpeg'
+  } 
+    
+
 
 
   return (
     <>
-     <Card nome='Rick Sanchez' foto="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-     <Card nome='Morty Smith' foto="https://rickandmortyapi.com/api/character/avatar/2.jpeg"/>
-     <Card nome='Summer Smith' foto="https://rickandmortyapi.com/api/character/avatar/3.jpeg"/>
-     <Card nome='Johnny Depp' foto="https://rickandmortyapi.com/api/character/avatar/23.jpeg"/>
+      <Card info= {item1}/>
+      <Card info= {item2}/>
+      <Card info= {item3}/>
+      <Card info= {item4}/>
+      
     </>
    
   )
