7e542eddaae34f258d4aaa29e6136bb4ecaa7ccc
Lucas <lucasbriansilvafagundes@gmail.com>
2025-07-31 19:49:55 -0300

    inplementação da array no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,35 @@ import './App.css'
 import Card from './card'
 
 function App() {
-  const item1 ={
-    nome:'Rick Sanchez', 
-    foto:'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  const item1 = {
+    nome: 'Rick Sanchez',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
-  const item2 ={
-    nome:'Morty smith', 
-    foto:'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  const item2 = {
+    nome: 'Morty smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
-    const item3 ={
-    nome:'Summer smith', 
-    foto:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  } 
-  const item4 ={
-    nome:'Johnny Depp', 
-    foto:'https://rickandmortyapi.com/api/character/avatar/23.jpeg'
-  } 
-    
+  const item3 = {
+    nome: 'Summer smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+  const item4 = {
+    nome: 'Johnny Depp',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/23.jpeg'
+  }
 
+  const personagens =  [item1, item2, item3, item4] 
 
 
   return (
     <>
-      <Card info= {item1}/>
-      <Card info= {item2}/>
-      <Card info= {item3}/>
-      <Card info= {item4}/>
-      
+      {
+        personagens.map(function (umpersonagem) {
+          return <Card info={umpersonagem} />
+        })
+      }
     </>
-   
+
   )
 }
 
